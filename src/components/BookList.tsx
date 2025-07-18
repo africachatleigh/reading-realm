206000b2649011d40fa45dc053e0ea23dbabfba1
africachatleigh <africachatleigh@gmail.com>
2025-07-18 15:18:57 +0100

    Updated BookList.tsx

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -98,7 +98,7 @@ const BookList: React.FC<BookListProps> = ({
       case '✨ Affo':
       case 'Affo': // Keep backwards compatibility
         return { backgroundColor: '#98e1eb', color: '#39929e' };
-      case '🧙‍♀️ Lou Lou':
+      case 'Lou Lou 🍁':
       case 'Lou Lou': // Keep backwards compatibility
         return { backgroundColor: '#f5af69', color: '#b86009' };
       case '🪄 Chlo':
